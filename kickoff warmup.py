7fae871d2c30eed3ffd13915aec715ef98ab485d
bobertbrown99 <158201038+bobertbrown99@users.noreply.github.com>
2024-01-31 17:26:40 +0000

    buffalo chicken dip

---
+++ kickoff warmup.py
@@ -0,0 +1,12 @@
+"I think computer program is something thats a specific human being created only him or her is able to altar
+"computer programs are very useful and we use it everyday
+#what is the best movie of 2023? spider man into the spider across the spider verse 
+#my favorite food is buffalo dip
+'how to make buffalo dip
+"step 1 Preheat oven to 350 degrees and grease a 9″ pie dish (this one is so pretty!). Then in a large bowl, add shredded chicken
+"step 2 Turn mixture out into prepared pie dish, smoothing top lightly with a spatula. Sprinkle with remaining cheese
+"step 3 put the shredded chicken in the oven with some hot sauce and cheese over it
+"step 4 while your doing that get a bowl of butter
+"step 5 take the shedded chicken out the oven
+"step 6 put some garlic salt over it 
+"step 7 sit down and enjoy the buffalo dip with some tostios scoops
--- test.py
+++ test.py
@@ -1,3 +1,3 @@
 # string = characters,text number, words wrapped in quotation mark. 
 print("ryder brown")
-
+"writen as string"
