680124b042ed477546eb40eac95055e16ef6df7d
bobertbrown99 <158201038+bobertbrown99@users.noreply.github.com>
2024-02-16 17:35:23 +0000

    "git push"

---
+++ Ryder Test.py
@@ -0,0 +1,59 @@
+#1. What built-in function would you use if you wanted
+# to print out data to a user?
+
+#2. What built-in function would you use if you wanted 
+# a user to pass in data to a program?
+
+#3. Explain what complexity and abstraction are, then 
+# provide an example describing their relationship to one another.
+# You cannot use the following examples because they 
+# were shared in class; grocery/ electronics store, car, phone. 
+#
+#4. What does it mean when you encounter a syntax error?
+#answer:it means that the computer is having problems understanding what your trying to do
+#5. What is a variable? Provide an example of a variable in code.
+#answer: a variable is a container for the data
+#6. Write a function in Python that when called will print out 
+# your name.
+#7. What is a function definition and a function call?
+
+#8. Describe what each symbol means?
+
+# = // single equal
+# == // double equal
+# != // exclaimation equal
+#answer:one equal is like a answer 2 equals mean there equal to eachother exclaimation equal means not equal
+#9. What will the output for the following code block be?
+var1 = 1
+var2 = 2
+var3 = "3"
+
+print(var1 + var2 + var3)
+#answer:it will add them together because your putting the numbers in there
+
+#10. What will the output for the following code block be?
+listOne = [20, 40, 60, 80]
+listTwo = [20, 40, 60, 80]
+
+print(listOne == listTwo)
+#answer:it will put the first list of numbers and then put the second list of numbers in those ()
+# 11. Create the following python lists and include the following
+# data types for each list:
+# a- create a python list of strings. The strings should be items you
+# can find in a book bag.
+# b- Create a python list of floats. the floats should represent the 
+# price of each item in a book bag from the previous list.
+
+# 12. What are the Three (3) naming formats we use for naming variables, files, and functions?
+
+# 13. Provide a coding scenario where you would you use a comparison operator in a program?
+#answer:80 == 80
+# 14. Provide a coding scenario Where you would you use a logical operator in a program?
+#answer: print("x<50 and x<100")
+# 15. Create a block of code where you would 
+# use a comparison operator with the follwing data:
+currentScore= 100
+highScore=2300
+#answer:2300>100
+# Bonus : What does the type() function do?
+#answer:it makes it so the computer can understand what your trying to do
--- test.py
+++ test.py
@@ -1,3 +1,4 @@
 # string = characters,text number, words wrapped in quotation mark. 
 print("ryder brown")
 "writen as string"
+
