1b1d6cbddd21c01c69083affe3ab1846940f7daa
bobertbrown99 <158201038+bobertbrown99@users.noreply.github.com>
2024-02-21 17:52:20 +0000

    `

---
+++ feb21async.py
@@ -0,0 +1,9 @@
+#in my opinion the socartic method is just teaching and answer questions
+#its important to ask questions while coding so you can make sure you can get everything right
+
+#I feel like a data broker is something that takes data from people and resells that info
+#i think they do it so they can sell you products that would be more interesting for you
+
+# i feel like the actions that are enforced on certain racial groups because of our current society for example two black men arrested for a crime they didnt commit in 2019, and 2020
+# because since certain racial groups probably cant get a grasp of the way the algorithim speaks or because of color comfort
+# like important because of the society they have an impact on along with the imact on young coloured people Giving them someone to look up too
---
+++ warmup_feb20.py
@@ -0,0 +1,10 @@
+Laundry = ["dirty clothes","tidepods","hamper","dryer","washing machine"]
+def laundryIndex():
+    print(Laundry[3])
+    print('hamper')
+laundryIndex()
+def reverseName():
+    userName = input('Ryder')[::-1]
+    print(userName)
+reverseName()
+
