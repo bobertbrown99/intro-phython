bda6451b93257ee65459e017a3d1d9b9dfe8ba55
bobertbrown99 <158201038+bobertbrown99@users.noreply.github.com>
2024-03-01 17:57:10 +0000

    march1 work

--- Warmups/warmup_mar1.py
+++ Warmups/warmup_mar1.py
@@ -1,7 +1,13 @@
-grades = [60,70,80,90]
-def gradebook(gradescore):
-    if gradescore >= grades[3]:
-        print('this student has an A')
-    if gradescore < grades[3]:
-        print('this student does not have an A')
-  
+def gradeBook_Ia(gradeScore):
+    if gradeScore >= 90:
+        print('this student has a A')
+    elif gradeScore >= 80 and gradeScore < 89:
+        print('this student has a B')
+    elif gradeScore >= 70 and gradeScore < 79:
+        print('this student has a C')
+    elif gradeScore >= 60 and gradeScore < 69:
+        print('this student has a D')
+        elif gradeScore >=1 and gradeScore < 59:
+        print('this student has a F')
+    else:
+        print('this is not a valid grade')
