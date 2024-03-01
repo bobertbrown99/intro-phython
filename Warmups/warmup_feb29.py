91af32bff5ad44c7374a527986f22460a8d0fb76
bobertbrown99 <158201038+bobertbrown99@users.noreply.github.com>
2024-03-01 17:24:53 +0000

    yes

--- Warmups/warmup_feb29.py
+++ Warmups/warmup_feb29.py
@@ -1,11 +1,11 @@
-#in your own owrds describe what phython conditional statements are (if/ELSE); more specifically, try to describe how they work.
-#answer: basicallly conditional statements are just a way to guide your code and make your code act on what you want it to do 3
+#in your own words describe what python conditional statements are (if/ELSE); more specifically, try to describe how they work.
+#answer: basically conditional statements are just a way to guide your code and make your code act on what you want it to do 3
 
 def check_highscore():
-    userScore= input('what is your current score?)')
+    userScore = int(input('what is your current score? '))
     if userScore > 3000:
-     print('congrats. You got the high score!')
+        print('congrats. You got the high score!')
     else:
         print('sorry. you didnt get the high score try again')
 
-check_highscore(2000)
+check_highscore(3000)
---
+++ Warmups/warmup_mar1.py
@@ -0,0 +1,7 @@
+grades = [60,70,80,90]
+def gradebook(gradescore):
+    if gradescore >= grades[3]:
+        print('this student has an A')
+    if gradescore < grades[3]:
+        print('this student does not have an A')
+  
