70df37403266d7e70b3f5797a343ae261a1b2466
bobertbrown99 <158201038+bobertbrown99@users.noreply.github.com>
2024-03-19 15:35:50 +0000

    activity18

--- Warmups/activitys'/activity march 5th.py
+++ Warmups/activitys'/activity march 5th.py
@@ -1,78 +1,35 @@
-# Async Assignment March 3rd, 2024. 
+from __future__ import print_function
+def elevator(floor):
+    floors = {
+        'M': 'lobby',
+        'B': 'basement',
+        'R': 'rooftop',
+        1: 'gym',
+        2: 'restaurant',
+        3: 'workspace',
+        4: 'living quarters'
+    }
 
-# Complete and solve the following prompts.
-# Make sure to write down the clues and keywords you found.
-# (Make sure to include your clues and keywords to recieve full credit)
-
-# Commit and sync your work before the end of class. 
-# This will count toward your class activity grade. 
-
-# Prompt #1- Elevator Conditional Function Execercise.
-
-# You have been hired by a architeture firm to assist 
-# with developing their elevator system. They have asked 
-# you to develop a function that will accept a user's input. 
-# your program should ask the user to enter a floor number. 
-# When an user enters a number they will be sent to a specific 
-# floor in the building. If the user enters a floor number that does not
-# exist, your program should inform the user that the floor they entered
-# does not exist and to enter another number and to try agin.
-
-# The architects have given you the following 
-# lists of floors for their building:
-
-# M = 'lobby'
-# B = 'basement'
-# R = 'rooftop'
-# 1 = 'gym'
-# 2 = 'restuarant'
-# 3 = 'workspace'
-# 4 = 'living quarters'
-
-#___________________________________________________________
-
-# Prompt #2- Amusement Park Conditional Function
-# You have been hired by an amusement park to develop a function
-# that allows users to access their roller coasters by entering their age
-# and height. Your program should take the user's height and age as parameters.
-# Your client has very specific requirements for thier guests 
-# to ride their roller coasters and have provided you with the following 
-# conditions that they would like your program to check for. 
-
-# user must be atleast 5.2 or taller and atleast 14 years old or older 
-# in order to ride roller coaster 1. 
-
-# if the user is shorter than 5.2 but at least 14 years of age or older,
-# they can ride the roller coaster 2.
-
-# if the user is shorter than 5.2 and also younger than 14 years of age,
-# they can ride roller coaster 3. 
-
-# if the user enters information incorrectly, give them an error message
-# and tell them that they entered their information incorrectly. 
-M='lobby'
-B = 'basement'
-R = 'rooftop'
-G = 'gym'
-R = 'restaurant'
-W = 'workspace'
-L = 'living quarters'
-
-def elevator_floor(floor):
-    if floor == 'M':
-        print('You are on the lobby floor')
-    elif floor == 'B':
-        print('You are on the basement floor')
-    elif floor == 'R':
-        print('You are on the rooftop floor')
-    elif floor == 'G':
-        print('You are on the gym floor')
-    elif floor == 'R':
-        print('You are on the restaurant floor')
-    elif floor == 'W':
-        print('You are on the workspace floor')
-    elif floor == 'L':
-        print('You are on the living quarters floor')
+    if floor in floors:
+        return floors[floor]
     else:
-        print('The floor you entered does not exist. Please enter another floor number and try again.')
-        
+        return "The floor you entered does not exist. Please enter another number."
+
+user_input = input("Enter a floor number: ")
+print(elevator(user_input))
+
+def amusement_park(height, age):
+    if height >= 5.2 and age >= 14:
+        print('You can ride roller coaster 1.')
+    elif height < 5.2 and age >= 14:
+        print("You can ride roller coaster 2.")
+    elif height < 5.2 and age < 14:
+        print("You can ride roller coaster 3.")
+    else:
+        print("You entered your information incorrectly.")
+
+
+user_height = float(input("Enter your height in feet: "))
+user_age = int(input("Enter your age: "))
+amusement_park(user_height, user_age)
+print(result)
